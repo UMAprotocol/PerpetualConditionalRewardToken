5446fe4249fb94da1e0c60149ec7ff34c2d7f6e0
dhood <dhood@users.noreply.github.com>
2022-05-22 16:02:13 +1000

    Get Gelato demo to pay for itself from ETH balance

--- backend-token/contracts/GelatoDemo.sol
+++ backend-token/contracts/GelatoDemo.sol
@@ -1,39 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-//import {OpsReady} from "../vendor/gelato/OpsReady.sol";
-// import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import {OpsReady} from "./vendor/gelato/OpsReady.sol";
 
-// import { SafeERC20 } from 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.3/contracts/token/ERC20/SafeERC20.sol';
-
-/*
-contract OpsReady {
-    IOps opsGel;
-    address payable public immutable gelato;
-    address public constant ETH = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-
-    modifier onlyOps() {
-        require(msg.sender == address(opsGel), "OpsReady: onlyOps");
-        _;
-    }
-
-    constructor(address _ops) {
-        opsGel = IOps(_ops);
-        gelato = opsGel.gelato();
-    }
-
-    function _transfer(uint256 _amount, address _paymentToken) internal {
-        if (_paymentToken == ETH) {
-            (bool success, ) = gelato.call{value: _amount}("");
-            require(success, "_transfer: ETH transfer failed");
-        } else {
-            SafeERC20.safeTransfer(IERC20(_paymentToken), gelato, _amount);
-        }
-    }
-}
-*/
-/*
 interface IOps {
+    /*
     function createTaskNoPrepayment(
         address _execAddr,
         bytes4 _execSelector,
@@ -43,24 +14,15 @@ interface IOps {
     ) external returns(bytes32 task);
 
     function gelato() external view returns (address payable);
+    */
     function getFeeDetails() external view returns (uint256, address);
 }
-*/
-/*
-interface IOps {
-    function createTask(
-        address _execAddress,
-        bytes4 _execSelector,
-        address _resolverAddress,
-        bytes calldata _resolverData
-    ) public returns (bytes32 task);
-}
-*/
-contract Counter  {
+
+contract CounterPaysOwnFees is OpsReady {
     uint256 public count;
     uint256 public lastExecuted;
 
-    constructor(/*address payable _ops*/)  {}
+    constructor(address _ops) OpsReady(_ops) {}
   /*  
     function startTask() external {
         IOps.createTask(
@@ -71,6 +33,9 @@ contract Counter  {
         );
     }
     */
+    // Allow contract to receive ETH balance
+    receive() external payable {}
+
     function increaseCount(/*uint256 amount */ ) external  {
         require(
             ((block.timestamp - lastExecuted) > 180),
@@ -79,6 +44,12 @@ contract Counter  {
 
         count += 1;
         lastExecuted = block.timestamp;
+
+        // Pay for Gelato fees
+        uint256 fee;
+        address feeToken;
+        (fee, feeToken) = IOps(ops).getFeeDetails();
+        _transfer(fee, feeToken);
     }
     
     function checker()
