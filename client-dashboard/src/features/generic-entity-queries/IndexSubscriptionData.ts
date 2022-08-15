a7e51b257f4c82677f9ea3c7f6d1d0512bb7da45
dhood <dhood@users.noreply.github.com>
2022-08-15 18:13:40 +1000

    Remove event filter for IDA receive events

--- client-dashboard/src/features/generic-entity-queries/IndexSubscriptionData.ts
+++ client-dashboard/src/features/generic-entity-queries/IndexSubscriptionData.ts
@@ -44,13 +44,13 @@ export const mostRecentDistributionSince: Function = (blockNumber: number | unde
     } = sfSubgraph.useEventsQuery(
         {
             chainId: queryChainId,
-            filter: {
-                addresses_contains: [tokenContractAddress.toLowerCase()],
-                // Sent is triggered on ida.distribute, and is not called in the contract for any other reason.
-                name: "Sent",
-                // Only get events since subscription was created
-                blockNumber_gte: blockNumber?.toString() || "0",
-            },
+            filter: {},
+            //     addresses_contains: [tokenContractAddress.toLowerCase()],
+            //     // Sent is triggered on ida.distribute, and is not called in the contract for any other reason.
+            //     name: "Sent",
+            //     // Only get events since subscription was created
+            //     blockNumber_gte: blockNumber?.toString() || "0",
+            // },
             pagination: undefined,
             // Think this can only handle filter OR order..?
             // order: {
